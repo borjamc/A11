64b07bb5c1a0aa28e0a258bd09098819e7ea0d75
borja molina catalan <borja molina catalan>
2016-10-26 22:08:08 +0200

    first commit

---
+++ While.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio de while</title>
+</head>
+<body>
+<?php 
+$multiplicacion=2;
+$multiplicador=1;
+while (mutiplicacion < 21)
+echo "2 * $multiplicador = $multiplicacion"
+
+
+?>
+</body>
+</html>
